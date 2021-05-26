311a5898bd8275bd8a350c3f073f48051aee3765
matb-code <sudeephb19@gmail.com>
2021-05-27 00:22:38 +0545

    search symbol and go to the page

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome()
+
+
 driver.get('http://www.nepalstock.com/company/display/697')
 
 stock_symbol_input = driver.find_element_by_xpath("//div[@id='search']//input[@id='stock_symbol']")
 stock_symbol_input.send_keys('NIFRA')
 
+search_symbol = driver.find_element_by_xpath("//div[@id='search']//button[@id='search_symbol']")
+search_symbol.click()
 
-time.sleep(2)
+
+time.sleep(3)
 driver.close()
+
