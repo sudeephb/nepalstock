d948a1a1480ef45a6e66cc276318e8a051d0a141
matb-code <sudeephb19@gmail.com>
2021-05-27 00:13:38 +0545

    symbol input

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import time
 driver = webdriver.Chrome()
 driver.get('http://www.nepalstock.com/company/display/697')
 
-
+stock_symbol_input = driver.find_element_by_xpath("//div[@id='search']//input[@id='stock_symbol']")
+stock_symbol_input.send_keys('NIFRA')
 
 
 time.sleep(2)
